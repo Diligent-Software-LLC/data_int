5b8cf8ae4f18c4789eba7c4065a22be26f51537a
Bradley J. Tannor <bradleytannor@gmail.com>
2020-03-28 09:33:47 -0700

    Setup Complete
    Regenerated files, configured the gem specification, and pinned issue templates in the remote repository.

---
+++ lib/data_int.rb
@@ -0,0 +1,11 @@
+# Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
+# under the MIT License.
+
+require_relative "data_int/version"
+
+# DataInt.
+# @abstract
+#   $DESCRIPTION$.
+class DataInt
+end
+
