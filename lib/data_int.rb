87979b6bb35048e0cce92f932a00f72977a674a9
Bradley J. Tannor <bradleytannor@gmail.com>
2020-03-28 15:46:04 -0700

    GNU General Public License
    Updated License.

--- lib/data_int.rb
+++ lib/data_int.rb
@@ -1,5 +1,5 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
-# under the MIT License.
+# under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
 require_relative "data_int/version"
 
